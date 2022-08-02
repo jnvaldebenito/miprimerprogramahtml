d24a4973bfef8cdbbd4069b5aa98b716a90cd85e
Jonatan Valdebenito <nicolas.valdebenito.osses1@gmail.com>
2022-08-01 22:25:49 -0400

    ahi te hice los cambios que necesitabas bro :v

--- code.js
+++ code.js
@@ -2,16 +2,18 @@ const numero = document.querySelector("#number")//id
 const lista = document.querySelector("#lista")//id
 const boton = document.querySelector("#boton")//clase
 
-boton.addEventListener("click", () =>{
-    if (true){
-        console.log("asd")
-        for(i=0;i<=10;i++){
-            let li = document.createElement("LI")
-            li.innerHTML = `${numero.value} * ${i} = ${numero.value*i}`;
-            lista.appendChild(li)
-        }
+boton.addEventListener("click", () => {
+  while( lista.firstChild ){
+    lista.removeChild( lista.firstChild );
+  }
+  if (parseInt(numero.value) > 0) {
+    for (let i = 0; i <= 10; i++) {
+      let li = document.createElement("LI");
+      li.innerHTML = `${numero.value} * ${i} = ${numero.value * i}`;
+      lista.appendChild(li)
     }
-    else { // si la primera condicion no se cumple nos da un alert
-        alert("Completa todos los campos!!")
-    }
-})
+  } else {
+    // si la primera condicion no se cumple nos da un alert
+    alert("Debes ingresar un número mayor a 0 para multiplicar!!");
+  }
+});
